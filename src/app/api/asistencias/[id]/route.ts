dbb27332929ab00484ec2d500fdc252d95795956
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2025-12-14 15:02:35 +0100

    sc

--- src/app/api/asistencias/[id]/route.ts
+++ src/app/api/asistencias/[id]/route.ts
@@ -3,6 +3,8 @@ import { verifyToken } from '@/lib/jwt';
 import { prisma } from '@/lib/prisma';
 import { z } from 'zod';
 
+export const dynamic = 'force-dynamic';
+
 const asistenciaSchema = z.object({
   empleadoId: z.string(),
   checkIn: z.string(),
